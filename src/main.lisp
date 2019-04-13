32a133a383f5b90790f110cac19deb821f2140d0
adammcpherson <aomcpherson@gmail.com>
2019-04-12 22:47:43 -0500

    Fib, many, and values macro

--- src/main.lisp
+++ src/main.lisp
@@ -3,3 +3,17 @@
 (in-package :test-project)
 
 ;; blah blah blah.
+(defun fib (n)
+  "Return the nth Fibonacci number."
+  (if (< n 2)
+      n
+      (+ (fib (- n 1))
+         (fib (- n 2)))))
+
+;; Multiple return
+(defun many (n)
+  (values n (* n 2) (* n 3)))
+
+;; The while loop
+(defmacro while (condition &body body)
+  `(loop while ,condition do (progn ,@body)))
